e9033320f8e9e7cd663f7e04d9f189774178b39a
Darren Strash <darren.strash@gmail.com>
2020-04-13 10:56:02 -0400

    Code for 4/13

---
+++ 10am/Class30/car.py
@@ -0,0 +1,114 @@
+"""
+    car.py
+    Darren Strash
+    A car that moves when pressing a button!
+"""
+
+from cs110graphics import *
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 600
+
+
+class Car(EventHandler):
+    """ A car that moves when we click buttons. """
+
+    def __init__(self, win, width, length, cen):
+        EventHandler.__init__(self)
+
+        self._win = win
+        self._width = width
+        self._length = length
+        self._center = cen
+
+        self._color = "deeppink"
+
+        self._rect = Rectangle(self._win, self._width, self._length,
+                               self._center)
+        self._rect.set_fill_color(self._color)
+        self._rect.set_border_color("skyblue")
+        self._rect.set_depth(6)
+
+        self._right_light_center = (self._center[0] + self._width//3, \
+                                    self._center[1] + self._length//4)
+        self._right_light = Circle(self._win, self._width//12, self._right_light_center)
+        self._right_light.set_depth(5)
+        self._right_light.set_fill_color("lightgray")
+        self._win.add(self._right_light)
+
+        self._left_light_center = (self._center[0] + self._width//3, \
+                                   self._center[1] - self._length//4)
+        self._left_light = Circle(self._win, self._width//12, self._left_light_center)
+        self._left_light.set_depth(5)
+        self._left_light.set_fill_color("lightgray")
+        self._win.add(self._left_light)
+
+        self._rect.add_handler(self)
+        self._left_light.add_handler(self)
+        self._right_light.add_handler(self)
+
+        # Represent which direction the car is moving
+        # x = 1 is moving right, x = -1 is moving left
+        # y = 1 is moving down, y = -1 is moving up
+        self._heading_x = 1
+        self._heading_y = 0
+
+        self._win.add(self._rect)
+
+    def move(self, direction):
+        """ Move rectangle to the right/left 20 pixels."""
+        (x, y) = self._center
+        if direction == "forward":
+            self._center = (x + self._heading_x * 20, y)
+        elif direction == "backward":
+            self._center = (x - self._heading_x * 20, y)
+
+        self._right_light_center = (self._center[0] + self._width//3, \
+                                    self._center[1] + self._length//4)
+        self._left_light_center = (self._center[0] + self._width//3, \
+                                   self._center[1] - self._length//4)
+
+        self._rect.move_to(self._center)
+        self._right_light.move_to(self._right_light_center)
+        self._left_light.move_to(self._left_light_center)
+
+
+class Button(EventHandler):
+    """ A button that when clicked will make a car move in a given direction """
+    def __init__(self, win, car, radius, center, direction):
+        """ Initialize the buttion's attributes. """
+        EventHandler.__init__(self)
+        self._win = win
+        self._car = car
+        self._radius = radius
+        self._center = center
+        self._direction = direction
+        self._circle = Circle(win, radius, center)
+        self._circle.set_fill_color("lightgray")
+        self._win.add(self._circle)
+        self._circle.add_handler(self)
+
+    def handle_mouse_press(self, event):
+        """ Change the button color and move the car when pressed. """
+        self._circle.set_fill_color("green")
+        self._car.move(self._direction)
+
+    def handle_mouse_release(self, event):
+        """ Change the button color back when releasing mouse buttion. """
+        self._circle.set_fill_color("lightgray")
+
+def main(win):
+    """The main function."""
+
+    win.set_width(WIN_WIDTH)
+    win.set_height(WIN_HEIGHT)
+
+    # Make car and buttons that control its movement
+    car = Car(win, 100, 40, (100, 500))
+    forward_button = Button(win, car, 15, (100, 20), "forward")
+    backward_button = Button(win, car, 15, (100, 50), "backward")
+
+
+if __name__ == '__main__':
+    """When using cs110graphics, replace the usual line with this one:"""
+    StartGraphicsSystem(main)
